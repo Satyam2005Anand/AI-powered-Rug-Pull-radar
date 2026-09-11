34e7d6a9c06d083a5754b6bf0f49c2cae86f9d5e
Satyam Anand <satyam20anand05@gmail.com>
2026-09-12 00:13:49 +0530

    Add four risk indicators

--- main.py
+++ main.py
@@ -74,6 +74,48 @@ def get_risk(token_address: str):
     else:
         pool_age_days = None
 
+    volume_24h_usd = (pair.get("volume") or {}).get("h24") or 0
+
+    transactions_24h = (pair.get("txns") or {}).get("h24") or {}
+
+    buys_24h = transactions_24h.get("buys") or 0
+    sells_24h = transactions_24h.get("sells") or 0
+
+    total_transactions = buys_24h + sells_24h
+
+    if total_transactions > 0:
+        sell_ratio = sells_24h / total_transactions
+    else:
+        sell_ratio = None
+
+    if sell_ratio is None:
+        sell_pressure_risk = 100
+    elif sell_ratio >= 0.80:
+        sell_pressure_risk = 100
+    elif sell_ratio >= 0.65:
+        sell_pressure_risk = 75
+    elif sell_ratio >= 0.55:
+        sell_pressure_risk = 40
+    else:
+        sell_pressure_risk = 10
+
+    if liquidity_usd > 0:
+        volume_liquidity_ratio = volume_24h_usd / liquidity_usd
+    else:
+        volume_liquidity_ratio = None
+
+
+    if volume_liquidity_ratio is None:
+        volume_risk = 100
+    elif volume_liquidity_ratio > 10:
+        volume_risk = 100
+    elif volume_liquidity_ratio > 5:
+        volume_risk = 75
+    elif volume_liquidity_ratio > 2:
+        volume_risk = 40
+    else:
+        volume_risk = 10
+
     if pool_age_days is None:
         pool_age_risk = 100
     elif pool_age_days < 7:
@@ -95,8 +137,13 @@ def get_risk(token_address: str):
         liquidity_risk = 10
 
     risk_score = round(
-        (liquidity_risk + pool_age_risk) / 2
-    )
+    (
+        liquidity_risk
+        + pool_age_risk
+        + volume_risk
+        + sell_pressure_risk
+    ) / 4
+)
 
 
     if risk_score <= 30:
@@ -119,6 +166,12 @@ def get_risk(token_address: str):
         "pair_created_at": pair.get("pairCreatedAt"),
         "pool_age_days": pool_age_days,
         "pool_age_risk": pool_age_risk,
+        "volume_liquidity_ratio": volume_liquidity_ratio,
+        "volume_risk": volume_risk,
+        "buys_24h": buys_24h,
+        "sells_24h": sells_24h,
+        "sell_ratio": sell_ratio,
+        "sell_pressure_risk": sell_pressure_risk,
         "liquidity_risk": liquidity_risk,
         "risk_score": risk_score,
         "risk_level": risk_level,
